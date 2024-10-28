77f4e6b269554e263453503f11f7b3d2964e23c0
Bijaya Gurung <bijayagurung@192-168-1-9.tpgi.com.au>
2024-10-28 18:09:28 +1100

    chatbot app

---
+++ app/src/main/java/com/example/easybot/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.easybot
+
+object Constants {
+    val apiKey = "AIzaSyDGYqfrsLjzM0Q74a8D5I54cg579WMSmDw"
+}
---
+++ app/src/main/java/com/example/easybot/ui/MessageModel.kt
@@ -0,0 +1,6 @@
+package com.example.easybot.ui
+
+data class MessageModel(
+    val message: String,
+    val role: String
+)
